bb7b24f84ffd67a46e2e90fd09c67a165ea43bd4
Gabriel Messias <gabrielmessias@outlook.com>
2025-09-01 11:52:36 -0300

    criando class Conta com seus metodos e testando no Menu.ts

--- Menu.ts
+++ Menu.ts
@@ -1,10 +1,18 @@
 import leia = require('readline-sync');
 import { colors } from './src/util/Colors';
+import { Conta } from './src/model/Conta';
 
 export function main() {
 
     let opcao: number;
     let continuar: boolean = true;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
     
     do {
         console.log(colors.bg.black + colors.fg.blue + "*****************************************************");
