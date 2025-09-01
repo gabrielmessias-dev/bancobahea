a75c7a6828af574216391861741905de7f16a1d7
Gabriel Messias <gabrielmessias@outlook.com>
2025-09-01 13:09:11 -0300

    testando no menu.ts os novos models corrente e poupanca

--- Menu.ts
+++ Menu.ts
@@ -1,6 +1,8 @@
 import leia = require('readline-sync');
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
@@ -13,6 +15,21 @@ export function main() {
     conta.visualizar();
     conta.depositar(5000);
     conta.visualizar();
+
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
     
     do {
         console.log(colors.bg.black + colors.fg.blue + "*****************************************************");
